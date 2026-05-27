c3e46c7e21c63c40de6ad74c950d81b2ebcdb21e
WiSoft <wisoft33@gmail.com>
2026-05-28 02:09:40 +0300

    ..bestlady..

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [   
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('products/', views.product, name='products'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, "product.html", {"products": products})
+
 
 def about(request):
     return render(request, 'about.html')
