78cc1777fba37264cf14ce6f7baffe85ec7d4ab0
WiSoft <wisoft33@gmail.com>
2026-05-30 02:19:24 +0300

    ..bestlady..

--- myapp/models.py
+++ myapp/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='img/')
 
     def __str__(self):
         return self.name
